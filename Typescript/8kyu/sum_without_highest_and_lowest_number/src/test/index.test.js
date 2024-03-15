cd685134b1b843875d5c6a56c9808a088fc39232
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-15 17:31:20 +0100

    test

--- Typescript/8kyu/sum_without_highest_and_lowest_number/src/test/index.test.js
+++ Typescript/8kyu/sum_without_highest_and_lowest_number/src/test/index.test.js
@@ -4,4 +4,7 @@ describe('sum without highest and lowest number', () => {
     it('should return 16', () => {
         expect(sumArray([6, 2, 1, 8, 10])).toBe(16)
     })
+    it('should return 17', () => {
+        expect(sumArray([6, 0, 1, 10, 10])).toBe(17)
+    })
 })

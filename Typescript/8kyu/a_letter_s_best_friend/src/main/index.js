948d6f78200caf4cead62e5c7e8853fe2ea5bdb4
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-15 20:34:16 +0100

    bestFriend solved v2

--- Typescript/8kyu/a_letter_s_best_friend/src/main/index.js
+++ Typescript/8kyu/a_letter_s_best_friend/src/main/index.js
@@ -1,23 +1,22 @@
 "use strict";
+// export function bestFriend(txt: string, a: string, b: string): boolean {
+//     const array = txt.split('')
+//     let result = []
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.bestFriend = void 0;
+//     for (let i = 0; i < array.length; i++) {
+//         console.log(array[i], a, array[i + 1], b)
+//         if (array[i] === a) {
+//             if (array[i + 1] !== undefined) {
+//                 if (array[i + 1] === b) {
+//                     result.push('true')
+//                 } else result.push('false')
+//             } else result.push('false')
+//         }
+//     }
+//     return result.every((el) => el === 'true')
+// }
 function bestFriend(txt, a, b) {
-    const array = txt.split('');
-    let result = [];
-    for (let i = 0; i < array.length; i++) {
-        console.log(array[i], a, array[i + 1], b);
-        if (array[i] === a) {
-            if (array[i + 1] !== undefined) {
-                if (array[i + 1] === b) {
-                    result.push('true');
-                }
-                else
-                    result.push('false');
-            }
-            else
-                result.push('false');
-        }
-    }
-    return result.every((el) => el === 'true');
+    return txt.split(a).length === txt.split(a + b).length ? true : false;
 }
 exports.bestFriend = bestFriend;
--- Typescript/8kyu/a_letter_s_best_friend/src/main/index.ts
+++ Typescript/8kyu/a_letter_s_best_friend/src/main/index.ts
@@ -1,17 +1,20 @@
+// export function bestFriend(txt: string, a: string, b: string): boolean {
+//     const array = txt.split('')
+//     let result = []
+
+//     for (let i = 0; i < array.length; i++) {
+//         console.log(array[i], a, array[i + 1], b)
+
+//         if (array[i] === a) {
+//             if (array[i + 1] !== undefined) {
+//                 if (array[i + 1] === b) {
+//                     result.push('true')
+//                 } else result.push('false')
+//             } else result.push('false')
+//         }
+//     }
+//     return result.every((el) => el === 'true')
+// }
 export function bestFriend(txt: string, a: string, b: string): boolean {
-    const array = txt.split('')
-    let result = []
-
-    for (let i = 0; i < array.length; i++) {
-        console.log(array[i], a, array[i + 1], b)
-
-        if (array[i] === a) {
-            if (array[i + 1] !== undefined) {
-                if (array[i + 1] === b) {
-                    result.push('true')
-                } else result.push('false')
-            } else result.push('false')
-        }
-    }
-    return result.every((el) => el === 'true')
+    return txt.split(a).length === txt.split(a + b).length ? true : false
 }

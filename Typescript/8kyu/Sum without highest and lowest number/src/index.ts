6555e316e50c6fd4f19e7c4b9a9b4c23eb63a9a9
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-15 16:51:17 +0100

    move index.ts

--- Typescript/8kyu/Sum without highest and lowest number/src/index.ts
+++
@@ -1,8 +0,0 @@
-export function sumArray(array: number[] | null): number {
-    return array
-        ? array
-              .sort((a: number, b: number) => a - b)
-              .slice(1, -1)
-              .reduce((acc, curr) => curr + acc, 0)
-        : 0
-}

01d4af488026f29fbea308639f482940256d7e7c
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-18 18:17:35 +0100

    log without dates setup

--- Typescript/7kyu/free_cell_deal_generator/src/main/index.js
+++ Typescript/7kyu/free_cell_deal_generator/src/main/index.js
@@ -1,1 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deal = void 0;
+function deal(n) {
+    return '1';
+}
+exports.deal = deal;
---
+++ Typescript/7kyu/free_cell_deal_generator/src/main/index.ts
@@ -0,0 +1,3 @@
+export function deal(n: number): string {
+    return '1'
+}

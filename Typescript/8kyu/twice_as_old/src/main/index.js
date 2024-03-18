5ad0958cb1a4bbb25981098deae56defa51f7f54
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-18 17:03:00 +0100

    twice_as_old setup

---
+++ Typescript/8kyu/twice_as_old/src/main/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.twiceAsOld = void 0;
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return 0;
+}
+exports.twiceAsOld = twiceAsOld;

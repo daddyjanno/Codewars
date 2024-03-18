b33b2e10c4b39ac9206a7f55385426fea4208901
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-18 17:58:35 +0100

    twice as old refacto

--- Typescript/8kyu/twice_as_old/src/main/index.js
+++ Typescript/8kyu/twice_as_old/src/main/index.js
@@ -2,13 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.twiceAsOld = void 0;
 function twiceAsOld(dadYearsOld, sonYearsOld) {
-    let array = [];
-    for (let i = 0; i <= dadYearsOld; i++) {
-        array.push(i);
-    }
-    return Number(array
-        .filter((num) => dadYearsOld + num === (sonYearsOld + num) * 2 ||
-        dadYearsOld - num === (sonYearsOld - num) * 2)
-        .join());
+    // let array: number[] = []
+    // for (let i = 0; i <= dadYearsOld; i++) {
+    //     array.push(i)
+    // }
+    // return Number(
+    //     array
+    //         .filter(
+    //             (num) =>
+    //                 dadYearsOld + num === (sonYearsOld + num) * 2 ||
+    //                 dadYearsOld - num === (sonYearsOld - num) * 2
+    //         )
+    //         .join()
+    // )
+    return Math.abs(dadYearsOld - sonYearsOld * 2);
 }
 exports.twiceAsOld = twiceAsOld;
--- Typescript/8kyu/twice_as_old/src/main/index.ts
+++ Typescript/8kyu/twice_as_old/src/main/index.ts
@@ -1,16 +1,18 @@
 export function twiceAsOld(dadYearsOld: number, sonYearsOld: number): number {
-    let array: number[] = []
-    for (let i = 0; i <= dadYearsOld; i++) {
-        array.push(i)
-    }
+    // let array: number[] = []
+    // for (let i = 0; i <= dadYearsOld; i++) {
+    //     array.push(i)
+    // }
 
-    return Number(
-        array
-            .filter(
-                (num) =>
-                    dadYearsOld + num === (sonYearsOld + num) * 2 ||
-                    dadYearsOld - num === (sonYearsOld - num) * 2
-            )
-            .join()
-    )
+    // return Number(
+    //     array
+    //         .filter(
+    //             (num) =>
+    //                 dadYearsOld + num === (sonYearsOld + num) * 2 ||
+    //                 dadYearsOld - num === (sonYearsOld - num) * 2
+    //         )
+    //         .join()
+    // )
+
+    return Math.abs(dadYearsOld - sonYearsOld * 2)
 }

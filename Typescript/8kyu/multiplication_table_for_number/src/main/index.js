ef881433d523fa2dc15436c7ca754adb6bf3201d
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-20 12:09:30 +0100

    refacto

--- Typescript/8kyu/multiplication_table_for_number/src/main/index.js
+++ Typescript/8kyu/multiplication_table_for_number/src/main/index.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.multiTable = void 0;
+// export function multiTable(number: number): string {
+//     let result = ''
+//     result += `1 * ${number} = ${1 * number}`
+//     for (let i = 2; i <= 10; i++) {
+//         result += `\n${i} * ${number} = ${i * number}`
+//     }
+//     return result
+// }
 function multiTable(number) {
-    let result = '';
-    result += `1 * ${number} = ${1 * number}`;
-    for (let i = 2; i <= 10; i++) {
-        result += `\n${i} * ${number} = ${i * number}`;
-    }
-    return result;
+    return Array.from({ length: 10 }, (x, i) => `${i + 1} * ${number} = ${(i + 1) * number}`).join('\n');
 }
 exports.multiTable = multiTable;
--- Typescript/8kyu/multiplication_table_for_number/src/main/index.ts
+++ Typescript/8kyu/multiplication_table_for_number/src/main/index.ts
@@ -1,8 +1,14 @@
+// export function multiTable(number: number): string {
+//     let result = ''
+//     result += `1 * ${number} = ${1 * number}`
+//     for (let i = 2; i <= 10; i++) {
+//         result += `\n${i} * ${number} = ${i * number}`
+//     }
+//     return result
+// }
 export function multiTable(number: number): string {
-    let result = ''
-    result += `1 * ${number} = ${1 * number}`
-    for (let i = 2; i <= 10; i++) {
-        result += `\n${i} * ${number} = ${i * number}`
-    }
-    return result
+    return Array.from(
+        { length: 10 },
+        (x, i) => `${i + 1} * ${number} = ${(i + 1) * number}`
+    ).join('\n')
 }

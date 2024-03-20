4fb73f700c38cf5850e339e84567501a89d5ef57
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-20 18:16:34 +0100

    third angle of a triangle setup

---
+++ Typescript/8kyu/third_angle_of_a_triangle/src/main/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.thirsAngleOfATriangle = void 0;
+function thirsAngleOfATriangle() { }
+exports.thirsAngleOfATriangle = thirsAngleOfATriangle;

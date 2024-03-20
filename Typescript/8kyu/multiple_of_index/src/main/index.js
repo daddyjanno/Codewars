9f3dd57310ffea0f732b8656eae0d669589598c3
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-20 11:48:50 +0100

    refacto

--- Typescript/8kyu/multiple_of_index/src/main/index.js
+++ Typescript/8kyu/multiple_of_index/src/main/index.js
@@ -1,16 +1,24 @@
 "use strict";
+// export function multipleOfIndex(array: number[]): number[] {
+//     let result: number[] = []
+//     if (array[0] === 0) {
+//         result.push(array[0])
+//     }
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % i === 0) {
+//             result.push(array[i])
+//         }
+//     }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.multipleOfIndex = void 0;
+//     return result
+// }
+// export function multipleOfIndex(array: number[]): number[] {
+//     return array.filter(
+//         (value: number, index: number) => value % index === 0 || !value
+//     )
+// }
 function multipleOfIndex(array) {
-    let result = [];
-    if (array[0] === 0) {
-        result.push(array[0]);
-    }
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] % i === 0) {
-            result.push(array[i]);
-        }
-    }
-    return result;
+    return array.filter((x, idx) => x % idx === 0 || x === idx);
 }
 exports.multipleOfIndex = multipleOfIndex;
--- Typescript/8kyu/multiple_of_index/src/main/index.ts
+++ Typescript/8kyu/multiple_of_index/src/main/index.ts
@@ -1,13 +1,21 @@
-export function multipleOfIndex(array: number[]): number[] {
-    let result: number[] = []
-    if (array[0] === 0) {
-        result.push(array[0])
-    }
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] % i === 0) {
-            result.push(array[i])
-        }
-    }
+// export function multipleOfIndex(array: number[]): number[] {
+//     let result: number[] = []
+//     if (array[0] === 0) {
+//         result.push(array[0])
+//     }
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % i === 0) {
+//             result.push(array[i])
+//         }
+//     }
 
-    return result
+//     return result
+// }
+// export function multipleOfIndex(array: number[]): number[] {
+//     return array.filter(
+//         (value: number, index: number) => value % index === 0 || !value
+//     )
+// }
+export function multipleOfIndex(array: number[]): number[] {
+    return array.filter((x: number, idx: number) => x % idx === 0 || x === idx)
 }

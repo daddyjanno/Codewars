c5ca7a216a631966a285df9bc0988c00c25f8a1c
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-20 17:21:14 +0100

    refacto

--- Typescript/8kyu/array_plus_array/src/main/index.js
+++ Typescript/8kyu/array_plus_array/src/main/index.js
@@ -1,8 +1,16 @@
 "use strict";
+// export function arrayPlusArray(arr1: number[], arr2: number[]): number {
+//     return (
+//         arr1.reduce((curr, acc) => curr + acc) +
+//         arr2.reduce((curr, acc) => curr + acc)
+//     )
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.arrayPlusArray = void 0;
+// export function arrayPlusArray(arr1: number[], arr2: number[]): number {
+//     return [...arr1, ...arr2].reduce((curr, acc) => curr + acc)
+// }
 function arrayPlusArray(arr1, arr2) {
-    return (arr1.reduce((curr, acc) => curr + acc) +
-        arr2.reduce((curr, acc) => curr + acc));
+    return arr1.concat(arr2).reduce((curr, acc) => curr + acc);
 }
 exports.arrayPlusArray = arrayPlusArray;
--- Typescript/8kyu/array_plus_array/src/main/index.ts
+++ Typescript/8kyu/array_plus_array/src/main/index.ts
@@ -1,6 +1,14 @@
+// export function arrayPlusArray(arr1: number[], arr2: number[]): number {
+//     return (
+//         arr1.reduce((curr, acc) => curr + acc) +
+//         arr2.reduce((curr, acc) => curr + acc)
+//     )
+// }
+
+// export function arrayPlusArray(arr1: number[], arr2: number[]): number {
+//     return [...arr1, ...arr2].reduce((curr, acc) => curr + acc)
+// }
+
 export function arrayPlusArray(arr1: number[], arr2: number[]): number {
-    return (
-        arr1.reduce((curr, acc) => curr + acc) +
-        arr2.reduce((curr, acc) => curr + acc)
-    )
+    return arr1.concat(arr2).reduce((curr, acc) => curr + acc)
 }

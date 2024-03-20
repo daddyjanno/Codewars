a945f40cc32bc6ce824f96813029cd4ce05626ce
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-20 10:45:45 +0100

    cat years dog years refacto

--- Typescript/8kyu/cat_years_dog_years/src/main/index.js
+++ Typescript/8kyu/cat_years_dog_years/src/main/index.js
@@ -1,21 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.catYearsdogYears = void 0;
+// export function catYearsdogYears(years: number): number[] {
+//     let catYears: number = 0
+//     let dogYears: number = 0
+//     if (years < 2) {
+//         catYears = 15
+//         dogYears = 15
+//     } else if (years < 3) {
+//         catYears = 15 + 9
+//         dogYears = 15 + 9
+//     } else {
+//         catYears = 15 + 9 + (years - 2) * 4
+//         dogYears = 15 + 9 + (years - 2) * 5
+//     }
+//     return [years, catYears, dogYears]
+// }
 function catYearsdogYears(years) {
-    let catYears = 0;
-    let dogYears = 0;
-    if (years < 2) {
-        catYears = 15;
-        dogYears = 15;
-    }
-    else if (years < 3) {
-        catYears = 15 + 9;
-        dogYears = 15 + 9;
-    }
-    else {
-        catYears = 15 + 9 + (years - 2) * 4;
-        dogYears = 15 + 9 + (years - 2) * 5;
-    }
-    return [years, catYears, dogYears];
+    return [
+        years,
+        years === 1 ? 15 : 15 + 9 + (years - 2) * 4,
+        years === 1 ? 15 : 15 + 9 + (years - 2) * 5,
+    ];
 }
 exports.catYearsdogYears = catYearsdogYears;

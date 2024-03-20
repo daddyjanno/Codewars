9530a934e8837a790dcf05cf47126e12dee43109
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-20 09:56:24 +0100

    refacto

--- Typescript/8kyu/the_feast_of_many_beasts/src/main/index.js
+++ Typescript/8kyu/the_feast_of_many_beasts/src/main/index.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.feast = void 0;
+// export function feast(beast: string, dish: string): boolean {
+//     return (
+//         beast.split('').pop() === dish.split('').pop() &&
+//         beast.split('').shift() === dish.split('').shift()
+//     )
+// }
 function feast(beast, dish) {
-    return (beast.split('').pop() === dish.split('').pop() &&
-        beast.split('').shift() === dish.split('').shift());
+    return beast[0] === dish[0] && beast.slice(-1) === dish.slice(-1);
 }
 exports.feast = feast;
--- Typescript/8kyu/the_feast_of_many_beasts/src/main/index.ts
+++ Typescript/8kyu/the_feast_of_many_beasts/src/main/index.ts
@@ -1,6 +1,9 @@
+// export function feast(beast: string, dish: string): boolean {
+//     return (
+//         beast.split('').pop() === dish.split('').pop() &&
+//         beast.split('').shift() === dish.split('').shift()
+//     )
+// }
 export function feast(beast: string, dish: string): boolean {
-    return (
-        beast.split('').pop() === dish.split('').pop() &&
-        beast.split('').shift() === dish.split('').shift()
-    )
+    return beast[0] === dish[0] && beast.slice(-1) === dish.slice(-1)
 }

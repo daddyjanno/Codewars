c0e9bc6e9bf5a73545a991c9c4fb2090af74e102
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-22 16:26:44 +0100

    valid parenthese ok

--- Typescript/7kyu/valid_parentheses/src/main/index.js
+++ Typescript/7kyu/valid_parentheses/src/main/index.js
@@ -2,9 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.validParentheses = void 0;
 function validParentheses(str) {
+    let newStr = str;
     if (str === '')
         return true;
-    if (str.length % 2 !== 0)
+    if (str.length % 2 !== 0) {
         return false;
+    }
+    else {
+        for (let i = 1; i <= str.length; i++) {
+            newStr = newStr.replace('()', '');
+            if (!newStr.length) {
+                return true;
+            }
+        }
+        return newStr.length ? false : true;
+    }
 }
 exports.validParentheses = validParentheses;
--- Typescript/7kyu/valid_parentheses/src/main/index.ts
+++ Typescript/7kyu/valid_parentheses/src/main/index.ts
@@ -1,4 +1,15 @@
 export function validParentheses(str: string): boolean {
+    let newStr = str
     if (str === '') return true
-    if (str.length % 2 !== 0) return false
+    if (str.length % 2 !== 0) {
+        return false
+    } else {
+        for (let i = 1; i <= str.length; i++) {
+            newStr = newStr.replace('()', '')
+            if (!newStr.length) {
+                return true
+            }
+        }
+        return newStr.length ? false : true
+    }
 }

0f3ddd014f5a727c7cf9f7b3cad5e97acd413e64
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-22 13:56:33 +0100

    last survivor ok

--- Typescript/7kyu/last_survivor/src/main/index.js
+++ Typescript/7kyu/last_survivor/src/main/index.js
@@ -1,1 +1,12 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.lastSurvivor = void 0;
+function lastSurvivor(letters, coords) {
+    let result = '';
+    let lettersLeft = letters.split('');
+    for (let i = 0; i < coords.length; i++) {
+        lettersLeft.splice(coords[i], 1).join();
+    }
+    return lettersLeft.join();
+}
+exports.lastSurvivor = lastSurvivor;
---
+++ Typescript/7kyu/last_survivor/src/main/index.ts
@@ -0,0 +1,9 @@
+export function lastSurvivor(letters: string, coords: number[]): string {
+    let result = ''
+    let lettersLeft = letters.split('')
+
+    for (let i = 0; i < coords.length; i++) {
+        lettersLeft.splice(coords[i], 1).join()
+    }
+    return lettersLeft.join()
+}

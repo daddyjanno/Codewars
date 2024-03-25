599f47d1f568ccc436d643a2ade2eeea6d158556
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-25 15:51:00 +0100

    playing cards ok

--- Typescript/7kyu/playing_cards-draw_order_part1/src/main/index.js
+++ Typescript/7kyu/playing_cards-draw_order_part1/src/main/index.js
@@ -2,6 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.draw = void 0;
 function draw(deck) {
-    return [];
+    let result = [];
+    while (deck.length) {
+        result.push(deck[0]);
+        deck.shift();
+        deck.push(deck[0]);
+        deck.shift();
+    }
+    return result;
 }
 exports.draw = draw;
--- Typescript/7kyu/playing_cards-draw_order_part1/src/main/index.ts
+++ Typescript/7kyu/playing_cards-draw_order_part1/src/main/index.ts
@@ -1,3 +1,10 @@
 export function draw(deck: string[]): string[] {
-    return []
+    let result: string[] | undefined = []
+    while (deck.length) {
+        result.push(deck[0])
+        deck.shift()
+        deck.push(deck[0])
+        deck.shift()
+    }
+    return result
 }

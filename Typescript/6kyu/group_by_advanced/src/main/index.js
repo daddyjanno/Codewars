4de2cc6d1acca7723c0d3356c1baf2df1445b42f
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-28 12:12:14 +0100

    groupBy

---
+++ Typescript/6kyu/group_by_advanced/src/main/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.groupBy = void 0;
+function groupBy(array, classifier, downstream, accumulatorSupplier) {
+    return [];
+}
+exports.groupBy = groupBy;
---
+++ Typescript/6kyu/group_by_advanced/src/main/index.ts
@@ -0,0 +1,15 @@
+interface Array {
+    name: string
+    income: number
+    profession: string
+    age: number
+}
+
+export function groupBy(
+    array: Array,
+    classifier: string,
+    downstream: string,
+    accumulatorSupplier: number
+) {
+    return
+}

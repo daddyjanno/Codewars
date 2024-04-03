4e219be7ea32d6a7e570d7c00afe734542a1f197
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-04-03 15:32:53 +0200

    refacto

--- Typescript/7kyu/alphabetical_addition/src/main/index.js
+++ Typescript/7kyu/alphabetical_addition/src/main/index.js
@@ -1,48 +1,52 @@
 "use strict";
+// let table = [
+//     'a',
+//     'b',
+//     'c',
+//     'd',
+//     'e',
+//     'f',
+//     'g',
+//     'h',
+//     'i',
+//     'j',
+//     'k',
+//     'l',
+//     'm',
+//     'n',
+//     'o',
+//     'p',
+//     'q',
+//     'r',
+//     's',
+//     't',
+//     'u',
+//     'v',
+//     'w',
+//     'x',
+//     'y',
+//     'z',
+// ]
+// export function addLetters(...letters: string[]): string {
+//     if (!letters.length) {
+//         return 'z'
+//     }
+//     let numbersArray = []
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.addLetters = void 0;
-let table = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-];
+//     for (let i = 0; i < letters.length; i++) {
+//         numbersArray.push(table.indexOf(letters[i]) + 1)
+//     }
+//     let result = numbersArray.reduce((curr, acc) => curr + acc, 0)
+//     console.log(result, result % 26)
+//     return result % 26 === 0 && result < 27
+//         ? table[result - 1]
+//         : result % 26 === 0
+//         ? 'z'
+//         : table[(result % 26) - 1]
+// }
 function addLetters(...letters) {
-    if (!letters.length) {
-        return 'z';
-    }
-    let numbersArray = [];
-    for (let i = 0; i < letters.length; i++) {
-        numbersArray.push(table.indexOf(letters[i]) + 1);
-    }
-    let result = numbersArray.reduce((curr, acc) => curr + acc, 0);
-    console.log(result, result % 26);
-    return result % 26 === 0 && result < 27
-        ? table[result - 1]
-        : result % 26 === 0
-            ? 'z'
-            : table[(result % 26) - 1];
+    return String.fromCharCode((letters.reduce((acc, val) => acc + val.charCodeAt(0) - 96, 0) % 26 ||
+        26) + 96);
 }
 exports.addLetters = addLetters;

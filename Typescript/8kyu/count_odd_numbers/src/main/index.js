833c645b6f2d450bcdd979272514b28f4de4c08e
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-04-04 11:58:35 +0200

    refacto

--- Typescript/8kyu/count_odd_numbers/src/main/index.js
+++ Typescript/8kyu/count_odd_numbers/src/main/index.js
@@ -2,10 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.oddCount = void 0;
 function oddCount(n) {
-    let count = 0;
-    for (let i = n - 1; i > 0; i--) {
-        i % 2 !== 0 ? (count += 1) : count;
-    }
-    return count;
+    return Math.floor(n / 2);
 }
 exports.oddCount = oddCount;
+// export function oddCount(n: number): number {
+//     let count = 0
+//     for (let i = n - 1; i > 0; i--) {
+//         i % 2 !== 0 ? (count += 1) : count
+//     }
+//     return count
+// }
--- Typescript/8kyu/count_odd_numbers/src/main/index.ts
+++ Typescript/8kyu/count_odd_numbers/src/main/index.ts
@@ -1,7 +1,10 @@
 export function oddCount(n: number): number {
-    let count = 0
-    for (let i = n - 1; i > 0; i--) {
-        i % 2 !== 0 ? (count += 1) : count
-    }
-    return count
+    return Math.floor(n / 2)
 }
+// export function oddCount(n: number): number {
+//     let count = 0
+//     for (let i = n - 1; i > 0; i--) {
+//         i % 2 !== 0 ? (count += 1) : count
+//     }
+//     return count
+// }

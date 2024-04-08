d59f6cd6bd139ea6da54b28e841e06127b4ee258
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-04-08 16:56:57 +0200

    refacto

--- Typescript/7kyu/square_pi/src/main/index.js
+++ Typescript/7kyu/square_pi/src/main/index.js
@@ -2,11 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.squarePi = void 0;
 function squarePi(digit) {
-    const arrayOfPI = '31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'.split('');
-    let result = 0;
-    for (let i = 0; i < digit; i++) {
-        result += Math.pow(+arrayOfPI[i], 2);
-    }
-    return Math.ceil(Math.sqrt(result));
+    const PI = '31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679';
+    return Math.ceil(Math.sqrt(PI.slice(0, digit)
+        .split('')
+        .reduce((curr, acc) => curr + Math.pow(+acc, 2), 0)));
 }
 exports.squarePi = squarePi;
+// export function squarePi(digit: number): number {
+//     const arrayOfPI =
+//         '31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'.split(
+//             ''
+//         )
+//     let result = 0
+//     for (let i = 0; i < digit; i++) {
+//         result += Math.pow(+arrayOfPI[i], 2)
+//     }
+//     return Math.ceil(Math.sqrt(result))
+// }
--- Typescript/7kyu/square_pi/src/main/index.ts
+++ Typescript/7kyu/square_pi/src/main/index.ts
@@ -1,14 +1,25 @@
 export function squarePi(digit: number): number {
-    const arrayOfPI =
-        '31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'.split(
-            ''
+    const PI: string =
+        '31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'
+    return Math.ceil(
+        Math.sqrt(
+            PI.slice(0, digit)
+                .split('')
+                .reduce((curr, acc) => curr + Math.pow(+acc, 2), 0)
         )
-
-    let result = 0
-
-    for (let i = 0; i < digit; i++) {
-        result += Math.pow(+arrayOfPI[i], 2)
-    }
-
-    return Math.ceil(Math.sqrt(result))
+    )
 }
+// export function squarePi(digit: number): number {
+//     const arrayOfPI =
+//         '31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'.split(
+//             ''
+//         )
+
+//     let result = 0
+
+//     for (let i = 0; i < digit; i++) {
+//         result += Math.pow(+arrayOfPI[i], 2)
+//     }
+
+//     return Math.ceil(Math.sqrt(result))
+// }

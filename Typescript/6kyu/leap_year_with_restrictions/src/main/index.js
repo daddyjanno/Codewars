a5176ee8b3604965ddd7de310bd4a76d3732b10d
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-04-09 16:21:48 +0200

    leap years with restrictions ok

--- Typescript/6kyu/leap_year_with_restrictions/src/main/index.js
+++ Typescript/6kyu/leap_year_with_restrictions/src/main/index.js
@@ -1,1 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isLeapYear = void 0;
+const isLeapYear = (year) => year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0) ? true : false;
+exports.isLeapYear = isLeapYear;

6e70b92e6bf296f33b2a29cf124498645d494a62
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-04-09 16:10:59 +0200

    refacto

--- Typescript/7kyu/leap_years/src/main/index.js
+++ Typescript/7kyu/leap_years/src/main/index.js
@@ -2,15 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isLeapYear = void 0;
 function isLeapYear(year) {
-    if (year % 4 === 0) {
-        if (year % 100 === 0) {
-            if (year % 400 === 0) {
-                return true;
-            }
-            return false;
-        }
-        return true;
-    }
-    return false;
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
+        ? true
+        : false;
 }
 exports.isLeapYear = isLeapYear;
+// export function isLeapYear(year: number): boolean {
+//     if (year % 4 === 0) {
+//         if (year % 100 === 0) {
+//             if (year % 400 === 0) {
+//                 return true
+//             }
+//             return false
+//         }
+//         return true
+//     }
+//     return false
+// }

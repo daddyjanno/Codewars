79e782b4524d1ae242302135c53df72d6f749803
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-04-10 17:52:10 +0200

    ok for 2 time the same firstname

--- Typescript/6kyu/generate_chat_room_names/src/main/index.js
+++ Typescript/6kyu/generate_chat_room_names/src/main/index.js
@@ -5,18 +5,46 @@ function generateChatRoomNames(users) {
     if (users.length === 0)
         return [];
     if (users.length === 1) {
-        let firstName = users[0].split(' ')[0].toLowerCase();
-        let lastName = users[0].split(' ')[1].toLowerCase();
-        return Array(firstName[0].toUpperCase() + firstName.substring(1));
+        let firstName = users[0].split(' ')[0][0].toUpperCase() +
+            users[0].split(' ')[0].substring(1).toLowerCase();
+        return Array(firstName);
     }
-    if (users.length > 1) {
-        let firstNames = [];
-        let lastNames = [];
-        users.forEach((user) => {
-            firstNames.push(user.split(' ')[0].toLowerCase());
-            lastNames.push(user.split(' ')[1].toLowerCase());
-            console.log(firstNames, lastNames);
-        });
+    users = users.sort();
+    let results = users.map((user) => user.split(' ')[0]);
+    for (let i = 0; i < results.length; i++) {
+        if (results[i] === results[i + 1]) {
+            console.log('same firstname');
+            results[i] = `${users[i].split(' ')[0]} ${users[i].split(' ')[1][0]}`;
+            results[i + 1] = `${users[i + 1].split(' ')[0]} ${users[i + 1].split(' ')[1][0]}`;
+            if (results[i] === results[i + 1]) {
+                results[i] = users[i];
+                results[i + 1] = users[i + 1];
+            }
+        }
     }
+    console.log('results :', results);
+    return results;
+    // users.forEach((user) => {
+    //     firstNames.push(
+    //         user.split(' ')[0][0].toUpperCase() +
+    //             user.split(' ')[0].substring(1).toLowerCase()
+    //     )
+    //     lastNames.push(
+    //         user.split(' ')[1][0].toUpperCase() +
+    //             user.split(' ')[1].substring(1).toLowerCase()
+    //     )
+    // })
+    // let result = []
+    // for (let i = 0; i < firstNames.length; i++) {
+    //     console.log(firstNames[i], firstNames[i + 1])
+    //     firstNames[i] !== firstNames[i + 1]
+    //         ? result.push(firstNames[i])
+    //         : `${firstNames[i]} ${lastNames[i][0]}` !==
+    //           `${firstNames[i + 1]} ${lastNames[i + 1][0]}`
+    //         ? result.push(`${firstNames[i]} ${lastNames[i][0]}`)
+    //         : result.push(`${firstNames[i]} ${lastNames[i]}`)
+    // }
+    // console.log(result)
+    // return result.sort()
 }
 exports.generateChatRoomNames = generateChatRoomNames;
--- Typescript/6kyu/generate_chat_room_names/src/test/index.test.js
+++ Typescript/6kyu/generate_chat_room_names/src/test/index.test.js
@@ -13,9 +13,24 @@ describe('Generate Chat Room Names, with one user', () => {
 })
 describe('Generate Chat Rooms, with many users', () => {
     it("should return user's firstnames if they are all unique", () => {
-        expect(generateChatRoomNames(['John Doe', 'Joe Blogs'])).toStrictEqual([
-            'John',
+        expect(generateChatRoomNames(['Joe Blogs', 'John Doe'])).toStrictEqual([
             'Joe',
+            'John',
         ])
+    })
+    it("should return user's firstnames if they are all unique, in alphabetical order", () => {
+        expect(
+            generateChatRoomNames(['John Doe', 'Joe Blogs', 'Jane Doe'])
+        ).toStrictEqual(['Jane', 'Joe', 'John'])
+    })
+    it("should return user's initials if their is a forename collision", () => {
+        expect(
+            generateChatRoomNames([
+                'Joe Bloggs',
+                'John Smith',
+                'Jane Doe',
+                'Jane Bloggs',
+            ])
+        ).toStrictEqual(['Jane B', 'Jane D', 'Joe', 'John'])
     })
 })

616d9095e36f956896c29c27cb475532b28b0953
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-04-11 14:51:47 +0200

    generateChatRooms ok!

--- Typescript/6kyu/generate_chat_room_names/src/main/index.ts
+++ Typescript/6kyu/generate_chat_room_names/src/main/index.ts
@@ -12,57 +12,23 @@ export function generateChatRoomNames(users: string[]): string[] {
     let firstNames = users.map((el) => el.split(' ')[0])
     let lastNames = users.map((el) => el.split(' ')[1])
 
-    console.log('users: ', users.sort(), users.length)
-    console.log('firstNames: ', firstNames, firstNames.length)
-    console.log('lastNames :', lastNames, lastNames.length)
-
     if (users.length === 0) return []
     if (users.length === 1) {
         return firstNames
     }
-    let results = [...users]
-    for (let i = 0; i < firstNames.length - 1; i++) {
-        if (firstNames[i] !== firstNames[i + 1]) {
-            results[i] = firstNames[i]
-        }
+    let results = [...firstNames]
+    for (let i = 0; i < results.length; i++) {
         if (firstNames[i] === firstNames[i + 1]) {
-            console.log('same firstNames :', users[i], users[i + 1])
-            if (lastNames[i][0] === lastNames[i + 1][0]) {
-                console.log('same initials', users[i], users[i + 1])
-                results[i] = firstNames[i] + ' ' + lastNames[i]
-                results[i + 1] = firstNames[i + 1] + ' ' + lastNames[i + 1]
-                console.log('result :', results[i], results[i + 1])
-            }
             results[i] = firstNames[i] + ' ' + lastNames[i][0]
             results[i + 1] = firstNames[i + 1] + ' ' + lastNames[i + 1][0]
-            console.log('result :', results[i], results[i + 1])
         }
     }
-    console.log(results)
+    for (let i = 0; i < firstNames.length; i++) {
+        if (results[i] === results[i + 1]) {
+            results[i] = users[i]
+            results[i + 1] = users[i + 1]
+        }
+    }
 
-    // for (let i = 0; i < usersInCap.length; i++) {
-    //     if (usersInCap[i].split(' ')[0] === usersInCap[i + 1].split(' ')[0]) {
-    //         if (
-    //             usersInCap[i].split(' ')[0] +
-    //                 ' ' +
-    //                 usersInCap[i].split(' ')[1][0] ===
-    //             usersInCap[i + 1].split(' ')[0] +
-    //                 ' ' +
-    //                 usersInCap[i + 1].split(' ')[1][0]
-    //         ) {
-    //             results[i] = usersInCap[i]
-    //             results[i + 1] = usersInCap[i + 1]
-    //         }
-    //         results[i] = `${usersInCap[i].split(' ')[0]} ${
-    //             usersInCap[i].split(' ')[1][0]
-    //         }`
-
-    //         results[i + 1] = `${usersInCap[i + 1].split(' ')[0]} ${
-    //             usersInCap[i + 1].split(' ')[1][0]
-    //         }`
-    //     }
-    // }
-    console.log('results after :', results, results.length)
-    //
     return results
 }

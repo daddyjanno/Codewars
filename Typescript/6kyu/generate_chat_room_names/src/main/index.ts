6a67f9d5745ad433fcb9c2711ff915136413770b
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-04-11 14:12:59 +0200

    tests sur un grand nombnre de cas

--- Typescript/6kyu/generate_chat_room_names/src/main/index.ts
+++ Typescript/6kyu/generate_chat_room_names/src/main/index.ts
@@ -1,59 +1,68 @@
 export function generateChatRoomNames(users: string[]): string[] {
+    users = users
+        .map(
+            (user) =>
+                user.split(' ')[0][0].toUpperCase() +
+                user.split(' ')[0].substring(1).toLowerCase() +
+                ' ' +
+                user.split(' ')[1][0].toUpperCase() +
+                user.split(' ')[1].substring(1).toLowerCase()
+        )
+        .sort()
+    let firstNames = users.map((el) => el.split(' ')[0])
+    let lastNames = users.map((el) => el.split(' ')[1])
+
+    console.log('users: ', users.sort(), users.length)
+    console.log('firstNames: ', firstNames, firstNames.length)
+    console.log('lastNames :', lastNames, lastNames.length)
+
     if (users.length === 0) return []
     if (users.length === 1) {
-        let firstName =
-            users[0].split(' ')[0][0].toUpperCase() +
-            users[0].split(' ')[0].substring(1).toLowerCase()
-
-        return Array(firstName)
+        return firstNames
     }
-
-    users = users.sort()
-
-    let results = users.map((user) => user.split(' ')[0])
-
-    for (let i = 0; i < results.length; i++) {
-        if (results[i] === results[i + 1]) {
-            console.log('same firstname')
-            results[i] = `${users[i].split(' ')[0]} ${
-                users[i].split(' ')[1][0]
-            }`
-            results[i + 1] = `${users[i + 1].split(' ')[0]} ${
-                users[i + 1].split(' ')[1][0]
-            }`
-            if (results[i] === results[i + 1]) {
-                results[i] = users[i]
-                results[i + 1] = users[i + 1]
+    let results = [...users]
+    for (let i = 0; i < firstNames.length - 1; i++) {
+        if (firstNames[i] !== firstNames[i + 1]) {
+            results[i] = firstNames[i]
+        }
+        if (firstNames[i] === firstNames[i + 1]) {
+            console.log('same firstNames :', users[i], users[i + 1])
+            if (lastNames[i][0] === lastNames[i + 1][0]) {
+                console.log('same initials', users[i], users[i + 1])
+                results[i] = firstNames[i] + ' ' + lastNames[i]
+                results[i + 1] = firstNames[i + 1] + ' ' + lastNames[i + 1]
+                console.log('result :', results[i], results[i + 1])
             }
+            results[i] = firstNames[i] + ' ' + lastNames[i][0]
+            results[i + 1] = firstNames[i + 1] + ' ' + lastNames[i + 1][0]
+            console.log('result :', results[i], results[i + 1])
         }
     }
-    console.log('results :', results)
+    console.log(results)
 
-    return results
+    // for (let i = 0; i < usersInCap.length; i++) {
+    //     if (usersInCap[i].split(' ')[0] === usersInCap[i + 1].split(' ')[0]) {
+    //         if (
+    //             usersInCap[i].split(' ')[0] +
+    //                 ' ' +
+    //                 usersInCap[i].split(' ')[1][0] ===
+    //             usersInCap[i + 1].split(' ')[0] +
+    //                 ' ' +
+    //                 usersInCap[i + 1].split(' ')[1][0]
+    //         ) {
+    //             results[i] = usersInCap[i]
+    //             results[i + 1] = usersInCap[i + 1]
+    //         }
+    //         results[i] = `${usersInCap[i].split(' ')[0]} ${
+    //             usersInCap[i].split(' ')[1][0]
+    //         }`
 
-    // users.forEach((user) => {
-    //     firstNames.push(
-    //         user.split(' ')[0][0].toUpperCase() +
-    //             user.split(' ')[0].substring(1).toLowerCase()
-    //     )
-    //     lastNames.push(
-    //         user.split(' ')[1][0].toUpperCase() +
-    //             user.split(' ')[1].substring(1).toLowerCase()
-    //     )
-    // })
-
-    // let result = []
-    // for (let i = 0; i < firstNames.length; i++) {
-    //     console.log(firstNames[i], firstNames[i + 1])
-
-    //     firstNames[i] !== firstNames[i + 1]
-    //         ? result.push(firstNames[i])
-    //         : `${firstNames[i]} ${lastNames[i][0]}` !==
-    //           `${firstNames[i + 1]} ${lastNames[i + 1][0]}`
-    //         ? result.push(`${firstNames[i]} ${lastNames[i][0]}`)
-    //         : result.push(`${firstNames[i]} ${lastNames[i]}`)
+    //         results[i + 1] = `${usersInCap[i + 1].split(' ')[0]} ${
+    //             usersInCap[i + 1].split(' ')[1][0]
+    //         }`
+    //     }
     // }
-    // console.log(result)
-
-    // return result.sort()
+    console.log('results after :', results, results.length)
+    //
+    return results
 }

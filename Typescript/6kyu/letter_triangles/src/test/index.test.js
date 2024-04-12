7fd503771798086894cd4f7fb60c20acfd6f17e8
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-04-12 16:34:20 +0200

    tests

--- Typescript/6kyu/letter_triangles/src/test/index.test.js
+++ Typescript/6kyu/letter_triangles/src/test/index.test.js
@@ -1,7 +1,13 @@
 const { triangle } = require('../main/index.js')
 
 describe('Letter Triangles tests', () => {
+    it('should return B', () => {
+        expect(triangle('B')).toBe('B')
+    })
     it('should return T', () => {
         expect(triangle('ABCD')).toBe('T')
+    })
+    it('should return L', () => {
+        expect(triangle('CODEWARS')).toBe('L')
     })
 })

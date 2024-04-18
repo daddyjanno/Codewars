701b7eb4bc3916404b82821467ee7a324511e3f7
daddyjanno <jn.drugmand@gmail.com>
2024-04-18 18:20:10 +0200

    Even or Odd

---
+++ C#/8kyu/Even or Odd/main.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Solution
+{
+    public class SolutionClass {
+        public static string EvenOrOdd(int number) {
+            return number % 2 == 0 ? "Even" : "Odd";
+        }
+    }
+}

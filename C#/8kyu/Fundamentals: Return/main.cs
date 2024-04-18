aec1ec132d92b49d38db877d99b4dcf58976eed7
daddyjanno <jn.drugmand@gmail.com>
2024-04-18 18:44:14 +0200

    main

---
+++ C#/8kyu/Fundamentals: Return/main.cs
@@ -0,0 +1,22 @@
+using System;
+
+public static class Kata {
+    public static int Add(int a, int b) {
+        return a + b;
+    }
+    public static int Multiply(int a, int b) {
+        return a * b;
+    }
+    public static int Divide(int a, int b) {
+        return a / b;
+    }
+    public static int Mod(int a, int b) {
+        return a % b;
+    }
+    public static int Exponent(int a, int b) {
+        return Math.Pow(a, b);
+    }
+    public static int Subt(int a, int b) {
+        return a - b;
+    }
+}

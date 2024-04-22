fa59db04b4a82e168b7b9167acab291df9a665ac
daddyjanno <jn.drugmand@gmail.com>
2024-04-22 14:40:19 +0200

    index

--- Typescript/7kyu/check_the_exam/src/main/index.js
+++ Typescript/7kyu/check_the_exam/src/main/index.js
@@ -1,1 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.checkExam = void 0;
+function checkExam(array1, array2) {
+    return 0;
+}
+exports.checkExam = checkExam;
---
+++ Typescript/7kyu/check_the_exam/src/main/index.ts
@@ -0,0 +1,3 @@
+export function checkExam(array1: string[], array2: string[]): number {
+    return 0
+}

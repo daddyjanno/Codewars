6c9e1f8c45459ec75d70cf0259780fb9e40b5f0c
daddyjanno <jn.drugmand@gmail.com>
2024-04-22 14:44:55 +0200

    chekc the exam solved

--- Typescript/7kyu/check_the_exam/src/main/index.js
+++ Typescript/7kyu/check_the_exam/src/main/index.js
@@ -2,6 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.checkExam = void 0;
 function checkExam(array1, array2) {
-    return 0;
+    let result = 0;
+    for (let index = 0; index < array1.length; index++) {
+        if (array2[index] === '') {
+            continue;
+        }
+        if (array1[index] === array2[index]) {
+            result += 4;
+        }
+        else
+            result += -1;
+    }
+    return result >= 0 ? result : 0;
 }
 exports.checkExam = checkExam;
--- Typescript/7kyu/check_the_exam/src/main/index.ts
+++ Typescript/7kyu/check_the_exam/src/main/index.ts
@@ -1,3 +1,12 @@
 export function checkExam(array1: string[], array2: string[]): number {
-    return 0
+    let result = 0
+    for (let index = 0; index < array1.length; index++) {
+        if (array2[index] === '') {
+            continue
+        }
+        if (array1[index] === array2[index]) {
+            result += 4
+        } else result += -1
+    }
+    return result >= 0 ? result : 0
 }

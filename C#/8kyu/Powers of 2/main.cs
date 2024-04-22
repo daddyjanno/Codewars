4d9b4801b3805e77c528da581da3a267289f57ec
daddyjanno <jn.drugmand@gmail.com>
2024-04-22 14:27:02 +0200

    main

--- C#/8kyu/Powers of 2/main.cs
+++ C#/8kyu/Powers of 2/main.cs
@@ -1,14 +1,12 @@
+using System.Numerics;
+using System.Linq;
 using System;
 
 public class Kata
 {
   public static BigInteger[] PowersOfTwo(int n)
   {
-    int[] result = []
-    for (int i = 0; i <= n; i++)
-    {
-        result[i] = Math.Pow(2, i);
-    }
-  return result;
+    return Enumerable.Range(0, n+1).Select(num => new BigInteger(Math.Pow(2.0, num))).ToArray();
+
   }
 }

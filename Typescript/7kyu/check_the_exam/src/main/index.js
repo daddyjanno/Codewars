8eca953fd1f0258cb4bdff7724695add46676ca7
daddyjanno <jn.drugmand@gmail.com>
2024-04-22 14:46:08 +0200

    refacto

--- Typescript/7kyu/check_the_exam/src/main/index.js
+++ Typescript/7kyu/check_the_exam/src/main/index.js
@@ -1,18 +1,27 @@
 "use strict";
+// export function checkExam(array1: string[], array2: string[]): number {
+//     let result = 0
+//     for (let index = 0; index < array1.length; index++) {
+//         if (array2[index] === '') {
+//             continue
+//         }
+//         if (array1[index] === array2[index]) {
+//             result += 4
+//         } else result += -1
+//     }
+//     return result >= 0 ? result : 0
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.checkExam = void 0;
 function checkExam(array1, array2) {
     let result = 0;
-    for (let index = 0; index < array1.length; index++) {
-        if (array2[index] === '') {
-            continue;
-        }
-        if (array1[index] === array2[index]) {
-            result += 4;
-        }
-        else
-            result += -1;
-    }
-    return result >= 0 ? result : 0;
+    array2.forEach((item, index) => {
+        item === array1[index]
+            ? (result += 4)
+            : item === ''
+                ? (result += 0)
+                : (result -= 1);
+    });
+    return Math.max(result, 0);
 }
 exports.checkExam = checkExam;
--- Typescript/7kyu/check_the_exam/src/main/index.ts
+++ Typescript/7kyu/check_the_exam/src/main/index.ts
@@ -1,12 +1,26 @@
+// export function checkExam(array1: string[], array2: string[]): number {
+//     let result = 0
+//     for (let index = 0; index < array1.length; index++) {
+//         if (array2[index] === '') {
+//             continue
+//         }
+//         if (array1[index] === array2[index]) {
+//             result += 4
+//         } else result += -1
+//     }
+//     return result >= 0 ? result : 0
+// }
+
 export function checkExam(array1: string[], array2: string[]): number {
     let result = 0
-    for (let index = 0; index < array1.length; index++) {
-        if (array2[index] === '') {
-            continue
-        }
-        if (array1[index] === array2[index]) {
-            result += 4
-        } else result += -1
-    }
-    return result >= 0 ? result : 0
+
+    array2.forEach((item, index) => {
+        item === array1[index]
+            ? (result += 4)
+            : item === ''
+            ? (result += 0)
+            : (result -= 1)
+    })
+
+    return Math.max(result, 0)
 }

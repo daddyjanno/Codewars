aa0db41f50647dc3f7c1087a34e2822988d817a3
daddyjanno <jn.drugmand@gmail.com>
2024-04-23 11:58:14 +0200

    bunch of tests

--- Typescript/7kyu/possibility_array/src/test/index.test.js
+++ Typescript/7kyu/possibility_array/src/test/index.test.js
@@ -4,4 +4,28 @@ describe('Tests if all possibilities', () => {
     it('should return true', () => {
         expect(isAllPossibilities([0, 1, 2, 3])).toBe(true)
     })
+    it('should return false', () => {
+        expect(isAllPossibilities([0, 2, 19, 4, 4])).toBe(false)
+    })
+    it('should return true', () => {
+        expect(isAllPossibilities([3, 2, 1, 0])).toBe(true)
+    })
+    it('should return false', () => {
+        expect(isAllPossibilities([1, 2, 3, 4])).toBe(false)
+    })
+    it('should return false', () => {
+        expect(isAllPossibilities([0, 2, 3])).toBe(false)
+    })
+    it('should return true', () => {
+        expect(isAllPossibilities([0])).toBe(true)
+    })
+    it('should return true', () => {
+        expect(isAllPossibilities([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])).toBe(true)
+    })
+    it('should return false', () => {
+        expect(isAllPossibilities([0, 1, 3, -2, 5, 4])).toBe(false)
+    })
+    it('should return false', () => {
+        expect(isAllPossibilities([1, -1, 2, -2, 3, -3, 6])).toBe(false)
+    })
 })

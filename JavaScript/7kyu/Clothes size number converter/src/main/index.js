b20b5c59fcccb43d71c51b636ea0b512e1e702cc
daddyjanno <jn.drugmand@gmail.com>
2024-07-10 16:35:12 +0200

    refacto

--- JavaScript/7kyu/Clothes size number converter/src/main/index.js
+++ JavaScript/7kyu/Clothes size number converter/src/main/index.js
@@ -1,31 +1,43 @@
 function sizeToNumber(size) {
-    const baseSizes = {
-        s: 36,
-        m: 38,
-        l: 40,
-    }
+    if (size === 's') return 36
+    if (size === 'm') return 38
+    if (size === 'l') return 40
 
-    if (typeof size !== 'string' || size === '') {
-        return null
-    }
-    const regex = /^(x*)([sml])$/
-    const match = size.match(regex)
-    if (!match) {
-        return null
-    }
-    const modifiers = match[1].length
-    const baseSize = match[2]
+    const x = size.match(/x/g)?.length
 
-    if (baseSize === 'm' && modifiers > 0) {
-        return null
-    }
+    if (/^x+s$/.test(size)) return 36 - x * 2
+    if (/^x+l$/.test(size)) return 40 + x * 2
 
-    let result = baseSizes[baseSize]
-    if (baseSize !== 'm') {
-        baseSize === 'l' ? (result += modifiers * 2) : (result -= modifiers * 2)
-    }
-
-    return result
+    return null
 }
+// function sizeToNumber(size) {
+//     const baseSizes = {
+//         s: 36,
+//         m: 38,
+//         l: 40,
+//     }
+
+//     if (typeof size !== 'string' || size === '') {
+//         return null
+//     }
+//     const regex = /^(x*)([sml])$/
+//     const match = size.match(regex)
+//     if (!match) {
+//         return null
+//     }
+//     const modifiers = match[1].length
+//     const baseSize = match[2]
+
+//     if (baseSize === 'm' && modifiers > 0) {
+//         return null
+//     }
+
+//     let result = baseSizes[baseSize]
+//     if (baseSize !== 'm') {
+//         baseSize === 'l' ? (result += modifiers * 2) : (result -= modifiers * 2)
+//     }
+
+//     return result
+// }
 
 module.exports = sizeToNumber

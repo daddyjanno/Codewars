54f2ca06909696b4fef0a8255c0cf6322b3d9750
daddyjanno <jn.drugmand@gmail.com>
2024-07-10 14:54:52 +0200

    refacto

--- JavaScript/7kyu/Difference Of Squares/src/main/index.js
+++ JavaScript/7kyu/Difference Of Squares/src/main/index.js
@@ -1,12 +1,22 @@
-const differenceOfSquares = (n) => {
-    let numbers = []
-    for (let i = 1; i <= n; i++) {
-        numbers.push(i)
-    }
-    const squareOfSums = numbers.reduce((cur, acc) => cur + acc, 0) ** 2
-    const sumOfSquares = numbers.reduce((cur, acc) => cur + acc ** 2, 0)
+// const differenceOfSquares = (n) => {
+//     let numbers = []
+//     for (let i = 1; i <= n; i++) {
+//         numbers.push(i)
+//     }
+//     const squareOfSums = numbers.reduce((cur, acc) => cur + acc, 0) ** 2
+//     const sumOfSquares = numbers.reduce((cur, acc) => cur + acc ** 2, 0)
 
-    return squareOfSums - sumOfSquares
+//     return squareOfSums - sumOfSquares
+// }
+const differenceOfSquares = (n) => {
+    let squareOfSums = 0
+    let sumOfSquares = 0
+    for (let i = 1; i <= n; i++) {
+        squareOfSums += i
+        sumOfSquares += i ** 2
+    }
+
+    return squareOfSums ** 2 - sumOfSquares
 }
 
 module.exports = differenceOfSquares

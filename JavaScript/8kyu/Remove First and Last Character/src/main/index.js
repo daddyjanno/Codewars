bddaea5e11961c9ee78891ddac3596e85c6900a9
daddyjanno <jn.drugmand@gmail.com>
2024-07-11 14:04:57 +0200

    refacto

--- JavaScript/8kyu/Remove First and Last Character/src/main/index.js
+++ JavaScript/8kyu/Remove First and Last Character/src/main/index.js
@@ -1,5 +1,8 @@
+// const removeChar = (str) => {
+//     return str.substring(1, str.length - 1)
+// }
 const removeChar = (str) => {
-    return str.substring(1, str.length - 1)
+    return str.slice(1, -1)
 }
 
 module.exports = removeChar

465f90cb9b1b73ac15043f4f9457f07799cafb16
daddyjanno <jn.drugmand@gmail.com>
2024-07-12 15:22:09 +0200

    refacto

--- JavaScript/7kyu/How many are smaller than me/src/main/index.js
+++ JavaScript/7kyu/How many are smaller than me/src/main/index.js
@@ -1,14 +1,23 @@
 function smaller(nums) {
-    const result = []
-    for (let i = 0; i < nums.length; i++) {
-        result.push(0)
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] > nums[j]) {
-                result[i] += 1
-            }
-        }
-    }
-    return result
+    return nums.map((num, index) => {
+        return nums.slice(index + 1).reduce((acc, num2) => {
+            if (num > num2) acc += 1
+            return acc
+        }, 0)
+    })
 }
+
+// function smaller(nums) {
+//     const result = []
+//     for (let i = 0; i < nums.length; i++) {
+//         result.push(0)
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] > nums[j]) {
+//                 result[i] += 1
+//             }
+//         }
+//     }
+//     return result
+// }
 
 module.exports = smaller

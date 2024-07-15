eead4bc837fdc47b6e7b6c65e248824c4739277b
daddyjanno <jn.drugmand@gmail.com>
2024-07-15 16:39:04 +0200

    refacto

--- JavaScript/7kyu/Number Pairs/src/main/index.js
+++ JavaScript/7kyu/Number Pairs/src/main/index.js
@@ -1,9 +1,12 @@
 function getLargerNumbers(arr1, arr2) {
-    const result = []
-    for (let i = 0; i < arr1.length; i++) {
-        result.push(arr1[i] > arr2[i] ? arr1[i] : arr2[i])
-    }
-    return result
+    return arr1.map((el, i) => Math.max(el, arr2[i]))
 }
+// function getLargerNumbers(arr1, arr2) {
+//     const result = []
+//     for (let i = 0; i < arr1.length; i++) {
+//         result.push(arr1[i] > arr2[i] ? arr1[i] : arr2[i])
+//     }
+//     return result
+// }
 
 module.exports = getLargerNumbers

16910b8bfe452bfc06695c3875078d04ed077a12
daddyjanno <jn.drugmand@gmail.com>
2024-07-16 17:21:54 +0200

    refacto

--- JavaScript/5kyu/Not very secure/src/main/index.js
+++ JavaScript/5kyu/Not very secure/src/main/index.js
@@ -1,13 +1,17 @@
 function alphanumeric(string) {
-    const regex = /[a-zA-Z0-9]/
-    let result = false
-    if (!string) return false
-    for (letter of string) {
-        if (regex.test(letter)) {
-            result = true
-        } else return false
-    }
-    return result
+    const regex = /[a-z\d]+$/i
+    return regex.test(string)
 }
+// function alphanumeric(string) {
+//     const regex = /[a-zA-Z0-9]/
+//     let result = false
+//     if (!string) return false
+//     for (letter of string) {
+//         if (regex.test(letter)) {
+//             result = true
+//         } else return false
+//     }
+//     return result
+// }
 
 module.exports = alphanumeric

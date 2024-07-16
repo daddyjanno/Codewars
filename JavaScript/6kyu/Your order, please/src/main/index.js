c503934144f829a1b932acefd5762e21e8c15941
daddyjanno <jn.drugmand@gmail.com>
2024-07-16 16:48:24 +0200

    refacto

--- JavaScript/6kyu/Your order, please/src/main/index.js
+++ JavaScript/6kyu/Your order, please/src/main/index.js
@@ -1,12 +1,20 @@
-function order(words) {
-    if (!words) return ''
-    let result = []
-    const wordsArray = words.split(' ')
-    for (let i = 0; i < wordsArray.length; i++) {
-        result.push(wordsArray.find((el) => el.includes((i + 1).toString())))
-    }
+// function order(words) {
+//     if (!words) return ''
+//     let result = []
+//     const wordsArray = words.split(' ')
+//     for (let i = 0; i < wordsArray.length; i++) {
+//         result.push(wordsArray.find((el) => el.includes((i + 1).toString())))
+//     }
 
-    return result.join(' ')
+//     return result.join(' ')
+// }
+function order(words) {
+    return words
+        .split(' ')
+        .sort(function (a, b) {
+            return a.match(/\d/) - b.match(/\d/)
+        })
+        .join(' ')
 }
 
 module.exports = order

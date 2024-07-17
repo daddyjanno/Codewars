5ab8468f918158bbc8b0900d68de0ddd86d20275
daddyjanno <jn.drugmand@gmail.com>
2024-07-17 15:20:34 +0200

    solved

--- JavaScript/7kyu/Count the divisors of a number/src/main/index.js
+++ JavaScript/7kyu/Count the divisors of a number/src/main/index.js
@@ -1,5 +1,14 @@
 function getDivisorsCnt(n) {
-    return
+    let result = 0
+    if (n % Math.sqrt(n) === 0) {
+        result++
+    }
+    for (let i = 0; i < Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            result += 2
+        }
+    }
+    return result
 }
 
 module.exports = getDivisorsCnt

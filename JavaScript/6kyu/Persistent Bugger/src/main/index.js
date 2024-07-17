3d5d1a8bfeb2f2fed64d221045c1abbe5e64542c
daddyjanno <jn.drugmand@gmail.com>
2024-07-17 15:48:46 +0200

    refacto

--- JavaScript/6kyu/Persistent Bugger/src/main/index.js
+++ JavaScript/6kyu/Persistent Bugger/src/main/index.js
@@ -1,13 +1,25 @@
 function persistence(num) {
     let result = 0
-    let numbersArray = num.toString().split('')
-    while (numbersArray.length > 1) {
-        numbersArray = numbersArray
-            .reduce((cur, acc) => cur * acc)
+
+    while (num.toString().length > 1) {
+        num = num
             .toString()
             .split('')
+            .reduce((cur, acc) => cur * acc)
         result++
     }
     return result
 }
+// function persistence(num) {
+//     let result = 0
+//     let numbersArray = num.toString().split('')
+//     while (numbersArray.length > 1) {
+//         numbersArray = numbersArray
+//             .reduce((cur, acc) => cur * acc)
+//             .toString()
+//             .split('')
+//         result++
+//     }
+//     return result
+// }
 module.exports = persistence

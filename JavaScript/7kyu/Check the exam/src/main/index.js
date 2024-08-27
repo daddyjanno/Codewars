86f511b98831741346429b02068d2786961e6229
daddyjanno <jn.drugmand@gmail.com>
2024-08-27 17:07:11 +0200

    solved

--- JavaScript/7kyu/Check the exam/src/main/index.js
+++ JavaScript/7kyu/Check the exam/src/main/index.js
@@ -1,5 +1,13 @@
 function checkExam(array1, array2) {
-    return null
+    let result = 0
+    for (let i = 0; i < array1.length; i++) {
+        array2[i] === ''
+            ? result
+            : array1[i] === array2[i]
+            ? (result += 4)
+            : (result -= 1)
+    }
+    return result < 0 ? 0 : result
 }
 
 module.exports = checkExam

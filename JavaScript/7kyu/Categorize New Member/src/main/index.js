75cc7e9d2bdad1d80496ce9515406796f7a6238a
daddyjanno <jn.drugmand@gmail.com>
2024-08-27 16:48:26 +0200

    refacto

--- JavaScript/7kyu/Categorize New Member/src/main/index.js
+++ JavaScript/7kyu/Categorize New Member/src/main/index.js
@@ -1,9 +1,14 @@
+// function openOrSenior(data) {
+//     const result = []
+//     data.forEach((el) =>
+//         result.push(el[0] >= 55 && el[1] > 7 ? 'Senior' : 'Open')
+//     )
+//     return result
+// }
 function openOrSenior(data) {
-    const result = []
-    data.forEach((el) =>
-        result.push(el[0] >= 55 && el[1] > 7 ? 'Senior' : 'Open')
+    return data.map(([age, handicap]) =>
+        age > 54 && handicap > 7 ? 'Senior' : 'Open'
     )
-    return result
 }
 
 module.exports = openOrSenior

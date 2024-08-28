519567898e64552e9fad1eff05f86f4e7dedc4d6
daddyjanno <jn.drugmand@gmail.com>
2024-08-28 18:22:14 +0200

    solved

--- JavaScript/5kyu/The hashtag generator/src/main/index.js
+++ JavaScript/5kyu/The hashtag generator/src/main/index.js
@@ -1,5 +1,11 @@
 function generateHashtag(string) {
-    return null
+    if (string === '' || string.trim() === '') return false
+
+    let result = string
+        .split(' ')
+        .map((el) => el.charAt(0).toUpperCase() + el.slice(1))
+
+    return result.join('').length >= 140 ? false : '#' + result.join('')
 }
 
 module.exports = generateHashtag

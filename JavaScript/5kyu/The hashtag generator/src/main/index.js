0f3e3ce529bed30cf190ca59a160138c96cd4798
daddyjanno <jn.drugmand@gmail.com>
2024-08-28 18:24:10 +0200

    refacto

--- JavaScript/5kyu/The hashtag generator/src/main/index.js
+++ JavaScript/5kyu/The hashtag generator/src/main/index.js
@@ -1,11 +1,23 @@
+// function generateHashtag(string) {
+//     if (string === '' || string.trim() === '') return false
+
+//     let result = string
+//         .split(' ')
+//         .map((el) => el.charAt(0).toUpperCase() + el.slice(1))
+
+//     return result.join('').length >= 140 ? false : '#' + result.join('')
+// }
 function generateHashtag(string) {
-    if (string === '' || string.trim() === '') return false
+    if (!string.trim()) return false
 
-    let result = string
-        .split(' ')
-        .map((el) => el.charAt(0).toUpperCase() + el.slice(1))
+    const result =
+        '#' +
+        string
+            .split(' ')
+            .map((el) => el.charAt(0).toUpperCase() + el.slice(1))
+            .join('')
 
-    return result.join('').length >= 140 ? false : '#' + result.join('')
+    return result.length > 140 ? false : result
 }
 
 module.exports = generateHashtag
